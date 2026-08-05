8fcaa70f1e7d127e6c57d1f517c692ea830893b0
amandachhen-netizen <amandachhen@gmail.com>
2026-08-05 13:34:02 +1000

    Created search engine targetting search input and h1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert('Hello');
+function runUserSearch(event) {
+    event.preventDefault();
+    let userCitySearch = document.querySelector("searchACity");
+
+    let userCityDisplay = document.querySelector("h1");
+    userCityDisplay.innerHTML = `${searchACity.value}`;
+}
+
+let userInput = document.querySelector("#searchForm");
+userInput.addEventListener("submit", runUserSearch);
